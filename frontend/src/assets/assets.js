16be717a5c95a38d6eb3d594c2d69a8bd17e7dbb
Tanisha Ojha <tanisha.ojha2004x2@gmail.com>
2025-08-16 01:27:01 +0530

    basic navbar w logo

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -5,10 +5,12 @@ import Medimate1 from './medimate1.svg'
 import Medimate2 from './medimate2.svg'
 import Medimate3 from './medimate3.svg'
 import Medimate4 from './medimate4.svg'
+import Medimate1_png from './medimate1.png'
+import Medimate3_png from './Medimate3.png' 
 
 export const assets = {
-    Medimate1, Medimate2, Medimate3, Medimate4, Medimate_bandaid, Medimate_hrt
+    Medimate1, Medimate2, Medimate3, Medimate4, Medimate_bandaid, Medimate_hrt,Medimate1_png
 }
 
-export {Medimate1,Medimate2,Medimate3,Medimate4,Medimate_bandaid,Medimate_hrt}
+export {Medimate1,Medimate2,Medimate3,Medimate4,Medimate_bandaid,Medimate_hrt,Medimate1_png,Medimate3_png}
 
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,27 +1,27 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import { Medimate2 } from '../assets/assets'
+import { Medimate1_png } from '../assets/assets'
 
 const NavBar = () => {
   return (
-    <div>
-      <img src={Medimate2} alt="img" />
-      <ul>
+    <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
+      <img className='w-65 cursor pointer' src={Medimate1_png} alt="img" />
+      <ul className='hidden md:flex items-start gap-5 font-medium'>
         <NavLink>
-          <li>HOME</li>
-          <hr />
+          <li className='py-1'>HOME</li>
+          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto'/>
         </NavLink>
         <NavLink>
-          <li>DOCTORS</li>
-          <hr />
+          <li className='py-1'>DOCTORS</li>
+          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto'/>
         </NavLink>
         <NavLink>
-          <li>ABOUT</li>
-          <hr />
+          <li className='py-1'>ABOUT</li>
+          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto'/>
         </NavLink>
         <NavLink>
-          <li>CONTACT</li>
-          <hr />
+          <li className='py-1'>CONTACT</li>
+          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto'/>
         </NavLink>
       </ul>
       <div>
