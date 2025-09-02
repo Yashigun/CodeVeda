c9da8e9ab568e491703308a6d96a7bc251753be6
Tanisha Ojha <tanisha.ojha2004x2@gmail.com>
2025-09-02 10:36:21 +0530

    re added about section

--- frontend/src/pages/contact.jsx
+++ frontend/src/pages/contact.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 const Contact = () => {
   return (
     <div className='w-full bg-secondary py-12 md:py-16 lg:py-20'>
-      
+      <div className=' max-w-7xl mx-auto px-6 md:px-10 lg:px-20'>
+        <div className='flex flex-col md:flex-row items-center gap-8 md:gap-12'>
+
+          {/* Left side */}
+          
+
+        </div>
+      </div>
     </div>
   )
 }
